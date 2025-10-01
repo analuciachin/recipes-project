34a7e546416af754a048f81369ebddc94c13ca06
Ana Lucia Chin <ananime@gmail.com>
2025-10-01 11:31:58 -0400

    Add loading, error and dynamic id to fetch recipe in recipe details component

--- src/RecipeDetails/RecipeDetails.jsx
+++ src/RecipeDetails/RecipeDetails.jsx
@@ -2,25 +2,33 @@ import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 
 export default function RecipeDetails(props) {
-  const [details, setDetails] = useState({});
+  const [details, setDetails] = useState(null);
   const { id } = useParams();
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
-    fetch(`https://dummyjson.com/recipes/2`)
+    fetch(`https://dummyjson.com/recipes/${id}`)
       .then((response) => response.json())
       .then((data) => {
         setDetails(data);
+        setLoading(false);
         console.log(data);
       })
-      .catch((error) => console.log(error));
+      .catch((error) => {
+        setLoading(false);
+        setError(error.message);
+        console.log(error);
+      });
   }, [id]);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error: {error}</p>;
+  if (!details) return <p>No recipe found.</p>;
 
   return (
     <>
-      <div>
-        RecipeDetails TEST
-        {details.name ? <p>{details.name}</p> : <p>Loading...</p>}
-      </div>
+      <div>{details && <p>{details.name}</p>}</div>
     </>
   );
 }
