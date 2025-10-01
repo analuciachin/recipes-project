3f06aeed76eb64729a9b7c7971919d8a857f54f1
Ana Lucia Chin <ananime@gmail.com>
2025-10-01 15:44:45 -0400

    Add style to recipe details component

--- src/RecipeDetails/RecipeDetails.jsx
+++ src/RecipeDetails/RecipeDetails.jsx
@@ -33,43 +33,49 @@ export default function RecipeDetails(props) {
         <div className="recipe-details__container">
           <h1>{details.name}</h1>
           <p className="recipe-details__information">
-            Prep time: {details.prepTimeMinutes} minutes
+            <span>Prep time:</span> {details.prepTimeMinutes} minutes
           </p>
           <p className="recipe-details__information">
-            Cook time: {details.cookTimeMinutes} minutes
+            <span>Cook time:</span> {details.cookTimeMinutes} minutes
           </p>
           <p className="recipe-details__information">
-            Servings: {details.servings}
+            <span>Servings:</span> {details.servings}
           </p>
           <p className="recipe-details__information">
-            Level of difficulty: {details.difficulty}
+            <span>Level of difficulty:</span> {details.difficulty}
           </p>
           <p className="recipe-details__information">
-            Cuisine: {details.cuisine}
+            <span>Cuisine:</span> {details.cuisine}
           </p>
           <p className="recipe-details__information">
-            Calories per serving: {details.caloriesPerServing}
+            <span>Calories per serving:</span> {details.caloriesPerServing}
           </p>
           <p className="recipe-details__information">
-            Meal type: {details.mealType}
+            <span>Meal type:</span> {details.mealType}
           </p>
           <img
             src={details.image}
             alt={details.name}
             className="recipe-details__image"
           />
-          <h2>Ingredients</h2>
-          <ul>
-            {details.ingredients.map((ingredient, index) => (
-              <li key={index}>{ingredient}</li>
-            ))}
-          </ul>
-          <h2>Instructions</h2>
-          <ul>
-            {details.instructions.map((instruction, index) => (
-              <li key={index}>{instruction}</li>
-            ))}
-          </ul>
+          <div className="recipe-details__preparation">
+            <div className="recipe-details__ingredients">
+              <h2>Ingredients</h2>
+              <ul>
+                {details.ingredients.map((ingredient, index) => (
+                  <li key={index}>{ingredient}</li>
+                ))}
+              </ul>
+            </div>
+            <div className="recipe-details__instructions">
+              <h2>Instructions</h2>
+              <ul>
+                {details.instructions.map((instruction, index) => (
+                  <li key={index}>{instruction}</li>
+                ))}
+              </ul>
+            </div>
+          </div>
         </div>
       )}
     </>
