aa8e7c0db38e389508f091682dd9ee32dac31fa3
Ana Lucia Chin <ananime@gmail.com>
2025-10-03 16:19:28 -0400

    Correct function name on favourite recipes component

--- src/FavouriteRecipes/FavouriteRecipes.jsx
+++ src/FavouriteRecipes/FavouriteRecipes.jsx
@@ -1,4 +1,5 @@
-export default function GetFavouriteRecipes(props) {
+export default function FavouriteRecipes(props) {
+  const { favourites } = props;
   return (
     <>
       <h1>Favourite Recipes</h1>
