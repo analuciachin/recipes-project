c26fcef48423b1f020586df9e6d153240ff53230
Ana Lucia Chin <ananime@gmail.com>
2025-10-07 16:31:18 -0400

    Add recipe info to favourite recipes component

--- src/FavouriteRecipes/FavouriteRecipes.jsx
+++ src/FavouriteRecipes/FavouriteRecipes.jsx
@@ -1,9 +1,11 @@
 import { useState, useEffect } from "react";
 import "./FavouriteRecipes.css";
+import { useNavigate } from "react-router-dom";
 
 export default function FavouriteRecipes(props) {
   const { recipes } = props;
   const [count, setCount] = useState(0);
+  const navigate = useNavigate();
 
   function countFavourite() {
     for (const item of recipes) {
@@ -24,22 +26,36 @@ export default function FavouriteRecipes(props) {
     <>
       <h1>Favourite Recipes</h1>
       <div className="recipe-list">
-        <ul>
-          {count > 0 && recipes ? (
-            recipes
-              .filter((recipe) => recipe.favourite === true)
-              .map((recipe) => (
-                <li key={recipe.id}>
-                  Name: {recipe.name}
-                  <div></div>
-                </li>
-              ))
-          ) : (
-            <p className="favourite-recipes__no-recipe-message">
-              No favourite recipes selected.
-            </p>
-          )}
-        </ul>
+        {count > 0 && recipes ? (
+          recipes
+            .filter((recipe) => recipe.favourite === true)
+            .map((recipe) => (
+              <div key={recipe.id} className="recipe-card">
+                <h3 className="recipe-card__name">{recipe.name}</h3>
+                <img
+                  src={recipe.image}
+                  alt={recipe.name}
+                  className="recipe-card__image"
+                />
+                <p className="recipe-card__cuisine">
+                  <span>Cuisine:</span> {recipe.cuisine}
+                </p>
+                <p className="recipe-card__meal-type">
+                  <span>Meal Type:</span> {recipe.mealType}
+                </p>
+                <p className="recipe-card__difficulty">
+                  <span>Difficulty:</span> {recipe.difficulty}
+                </p>
+                <button onClick={() => navigate(`/recipes/${recipe.id}`)}>
+                  See recipe
+                </button>
+              </div>
+            ))
+        ) : (
+          <p className="favourite-recipes__no-recipe-message">
+            No favourite recipes selected.
+          </p>
+        )}
       </div>
     </>
   );
